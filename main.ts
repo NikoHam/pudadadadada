d13a225b1f318a60ea816cf66776dc1157c8b086
NikoHam <58589799+NikoHam@users.noreply.github.com>
2022-04-21 13:51:29 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 11) {
-        pins.digitalWritePin(DigitalPin.P12, 1)
-        pins.digitalWritePin(DigitalPin.P13, 1)
-        pins.digitalWritePin(DigitalPin.P14, 1)
+        Svetlo = 1
     } else if (receivedNumber == 10) {
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.digitalWritePin(DigitalPin.P14, 0)
-    } else {
-    	
+        Svetlo = 0
     }
     if (receivedNumber == 20) {
         pins.digitalWritePin(DigitalPin.P14, 0)
@@ -25,7 +19,20 @@ radio.onReceivedNumber(function (receivedNumber) {
     	
     }
 })
+let Svetlo = 0
 radio.setGroup(20)
+let LED1 = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
+let LED2 = neopixel.create(DigitalPin.P2, 1, NeoPixelMode.RGB)
+let LED3 = neopixel.create(DigitalPin.P12, 1, NeoPixelMode.RGB)
+Svetlo = 0
 basic.forever(function () {
-	
+    if (1 == Svetlo) {
+        LED1.showColor(neopixel.colors(NeoPixelColors.White))
+        LED2.showColor(neopixel.colors(NeoPixelColors.White))
+        LED3.showColor(neopixel.colors(NeoPixelColors.White))
+    } else {
+        LED1.showColor(neopixel.colors(NeoPixelColors.Black))
+        LED2.showColor(neopixel.colors(NeoPixelColors.Black))
+        LED3.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
 })
